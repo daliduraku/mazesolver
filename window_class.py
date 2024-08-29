6a39dcd8fffc01ae954e8dff92d637c46f68a2ec
github_username_here <mesherbetmolla@gmail.com>
2024-08-28 22:15:38 -0400

    first commit

---
+++ window_class.py
@@ -0,0 +1,11 @@
+from tkinter import Tk, BOTH, Canvas
+
+
+class Window:
+    def __init__(self, width, height):
+        self.width = width
+        self.height = height
+        self._root = Tk()
+    
+    
+        
