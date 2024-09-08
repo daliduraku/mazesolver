53f38c9c2252d0cdc4961aad460dcb84db7e8358
daliduraku <mesherbetmolla@gmail.com>
2024-09-08 16:09:24 -0400

    Created points and lines and called draw_line method from window_class to create a triangle

--- main.py
+++ main.py
@@ -1,10 +1,25 @@
 from window_class import Window
+from line_class import (
+    Line,
+    Point,
+
+)
 
 def main():
     win = Window(800, 600)
+    
+    p1 = Point(100, 100)
+    p2 = Point(400, 400)
+    p3 = Point(500, 0)
+
+    l1 = Line(p1, p2)
+    l2 = Line(p1, p3)
+    l3 = Line(p2, p3)
+    
+    win.draw_line(l1, 'green')
+    win.draw_line(l2, 'green')
+    win.draw_line(l3, 'green')
     win.wait_for_close()
-
-
 
 
 main()
