c62c697989de92a18059002fd52c0766f07e7bf3
daliduraku <mesherbetmolla@gmail.com>
2024-09-09 22:03:04 -0400

    created method draw_move in the cell_class and updated main.py to test the method

--- cell_class.py
+++ cell_class.py
@@ -1,3 +1,4 @@
+import tkinter as Tk
 from line_class import (
     Line,
     Point,
@@ -32,3 +33,21 @@ class Cell:
         if self.has_bottom_wall:
             line = Line(Point(x1, y2), Point(x2, y2))
             self._win.draw_line(line)
+    
+    def draw_move(self, to_cell, undo=False):
+
+        # add x1, x2 and divide by 2. Do the same with the y coords to find the center of the self cell and to_cell 
+        x1 = (self._x1 + self._x2) / 2
+        y1 = (self._y1 + self._y2) / 2
+        x2 = (to_cell._x1 + to_cell._x2) / 2
+        y2 = (to_cell._y1 + to_cell._y2) / 2
+        
+
+        if not undo:
+            self._win.canvas.create_line(x1, y1, x2, y2, fill="red")
+        else:
+            self._win.canvas.create_line(x1, y1, x2, y2, fill="gray")
+
+
+
+
