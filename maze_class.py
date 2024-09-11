5feeb7fba38f98a7d18afded8ce775d1c368d026
daliduraku <mesherbetmolla@gmail.com>
2024-09-10 21:12:19 -0400

    Created the draw_cells method

--- maze_class.py
+++ maze_class.py
@@ -1,4 +1,4 @@
-
+from cell_class import Cell
 
 class Maze:
     def __init__(
@@ -18,7 +18,17 @@ class Maze:
         self.cell_size_x = cell_size_x
         self.cell_size_y = cell_size_y
         self.win = win
-        self._cells =
+        self._cells = []
 
-    def _create_cells(self):
+    def create_cells(self):
+        for i in range(self.num_rows):
+            inner_list = []
+            for j in range(self.num_cols):
+                inner_list.append(Cell(self.win))
+            self._cells.append(inner_list)
+    
+    def draw_cells(self, i, j):
+        x_position = self.x1 + j * self.cell_size_x
+        y_position = self.y1 + i * self.cell_size_y
+        self.win.canvas.create_rectangle(x_position, y_position, x_position + self.cell_size_x, y_position + self.cell_size_y, outline="black", fill="white")
 
